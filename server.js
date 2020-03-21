f1931fc7502937a6d1b76b7966cd6d358e26cc33
Jose Pieters <jose.pieters@Hotmail.com>
2020-03-20 21:21:28 -0400

    Fixed Port

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 1008;
-
+const PORT = process.env.PORT || 1008;
 const app = express();
 
 app.use(logger("dev"));
